5b872de23f0313b567501fba5883fed3244c795a
Ivan <ivan.lee@itstar.tw>
2024-07-01 14:23:56 +0800

    更改為絕對路徑

--- ios/Classes/Runner-Bridging-Header.h
+++ ios/Classes/Runner-Bridging-Header.h
@@ -1,2 +1,2 @@
-#import <QAPM/QAPM.h>
-#import <QAPM/QAPMLaunchProfile.h>
+#import <QAPM/../ios/qapm-sdk/QAPM.framework/Headers/QAPM.h>
+#import <QAPM/../ios/qapm-sdk/QAPM.framework/Headers/QAPMLaunchProfile.h>

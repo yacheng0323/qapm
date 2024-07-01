dc1579fd10cc7963aac44bf76ebebc5a8d8c756b
Ivan <ivan.lee@itstar.tw>
2024-07-01 14:43:26 +0800

    add runner-swift.h

--- ios/Classes/QapmPlugin.m
+++ ios/Classes/QapmPlugin.m
@@ -1,5 +1,10 @@
 #import "QapmPlugin.h"
 
+#if __has_include("qapmPlugin-Swift.h")
+#import "qapmPlugin-Swift.h"
+#else
+#import "Runner-Swift.h"
+#endif
 
 @implementation QapmPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {

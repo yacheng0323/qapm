2dd8461daf42dd768264d79e1b79be11e8a48728
Ivan <ivan.lee@itstar.tw>
2024-07-01 14:37:40 +0800

    取消導入 qapmPlugin-Swift.h

--- ios/Classes/QapmPlugin.m
+++ ios/Classes/QapmPlugin.m
@@ -1,10 +1,5 @@
 #import "QapmPlugin.h"
 
-#if __has_include(<qapm/QapmPlugin-Swift.h>)
-#import <qapm/QapmPlugin-Swift.h>
-#else
-#import "qapmPlugin-Swift.h"
-#endif
 
 @implementation QapmPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {

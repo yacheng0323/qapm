3ed25bcf8bf426391a36e91c60d90f3efa7bcb20
Ivan <ivan.lee@itstar.tw>
2024-07-02 16:15:05 +0800

    修改為可選類型

--- ios/Classes/QapmPlugin.swift
+++ ios/Classes/QapmPlugin.swift
@@ -48,18 +48,18 @@ public class QapmPlugin: NSObject, FlutterPlugin {
   }
 
   private func handleCustomEvent(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-   if let dict = call.arguments as? [String: Any],
-      let category = dict["category"] as? String,
-      let tags = dict["tags"] as? [String: String],
-      let values = dict["values"] as? [String: NSNumber] {
-      let eventUUID = QAPMUBSMonitor.manager().customEvent(category, tags: tags, values: values)
-      if let eventUUID = eventUUID {
-         result(eventUUID)
-      } else {
-         result(FlutterError(code: "EVENT_CREATION_FAILED", message: "Failed to create custom event", details: nil))
-      }
-   } else {
-      result(FlutterError(code: "INVALID_ARGUMENT", message: "Invalid argument", details: nil))
-   }
-}
+    if let dict = call.arguments as? [String: Any],
+       let category = dict["category"] as? String?,
+       let tags = dict["tags"] as? [String: String]?,
+       let values = dict["values"] as? [String: NSNumber]? {
+          if let category = category, let tags = tags, let values = values {
+            let eventUUID = QAPMUBSMonitor.manager().customEvent(category, tags: tags, values: values)
+            result(eventUUID)
+          } else {
+            result(FlutterError(code: "INVALID_ARGUMENT",message: "Invalid argument", details: nil))
+          }
+       } else {
+        result(FlutterError(code: "INVALID_ARGUMENT",message: "Invalid argument",details: nil))
+       }
+  }
 }
